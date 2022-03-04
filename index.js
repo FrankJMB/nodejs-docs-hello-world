a7b7064dad8a314242f9d9e5900060ba0b011f1e
FrankJMB <frank.beentjes@live.nl>
2022-03-04 23:13:51 +0100

    10

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! 9");
+    response.end("Hello World! 10");
 });
 
 const port = process.env.PORT || 1337;
